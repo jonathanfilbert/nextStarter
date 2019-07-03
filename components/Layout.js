287a8d9c3d795c23583994bcce32d7c4f6c5c609
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2019-07-04 00:32:46 +0700

    fixed

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Head from "./Head";
 
 class Layout extends React.Component {
   render() {
     return (
       <div>
+        <Head title="Your amazing app" />
         <Navbar />
         {this.props.children}
         <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "../components/Head";
 import Layout from "../components/Layout";
 import { Flex, Box } from "rebass";
 import styled from "styled-components";
@@ -12,7 +11,6 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <Head title="Your amazing app" />
         <Layout>
           <Body>
             <Flex flexWrap="wrap" justifyContent="center">
